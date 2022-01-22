bd8fdb874405af259535d97afa4284ad093cde68
Nasanjargal <nasaa1997@gmail.com>
2022-01-22 08:39:49 -0600

    fix update

--- LAB10/public/angularApp/src/app/job/job.component.ts
+++ LAB10/public/angularApp/src/app/job/job.component.ts
@@ -2,91 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { JobsService } from '../jobs.service';
 
 export class Location {
-  #_id: String;
-  #address: String;
-  #coordinates: Number[];
-  get _id() {
-    return this.#_id;
-  }
-  set _id(id: String) {
-    this.#_id = id;
-  }
-  get address() {
-    return this.#address;
-  }
-  set address(address: String) {
-    this.#address = address;
-  }
-  get coordinates() {
-    return this.#coordinates;
-  }
-  set coordinates(coordinates: Number[]) {
-    this.#coordinates = coordinates;
-  }
+  _id: String;
+  address: String;
+  coordinates: Number[];
   constructor(id: String, address: String, coordinates: Number[]) {
-    this.#_id = id;
-    this.#address = address;
-    this.#coordinates = coordinates;
+    this._id = id;
+    this.address = address;
+    this.coordinates = coordinates;
   }
 }
 
 export class Jobs {
-  #_id: String;
-  #title: String;
-  #salary: Number;
-  #location: Location;
-  #description: String;
-  #experience: String;
-  #skills: String[];
-  #postDate: Date;
-  get _id() {
-    return this.#_id;
-  }
-  set _id(id: String) {
-    this.#_id = id;
-  }
-  get title() {
-    return this.#title;
-  }
-  set title(title: String) {
-    this.#title = title;
-  }
-  get salary() {
-    return this.#salary;
-  }
-  set salary(salary: Number) {
-    this.#salary = salary;
-  }
-  get location(): Location {
-    return this.#location;
-  }
-  set location(location: Location) {
-    this.#location = location;
-  }
-  get description() {
-    return this.#description;
-  }
-  set description(description: String) {
-    this.#description = description;
-  }
-  get experience() {
-    return this.#experience;
-  }
-  set experience(experience: String) {
-    this.#experience = experience;
-  }
-  get skills() {
-    return this.#skills;
-  }
-  set skills(skills: String[]) {
-    this.#skills = skills;
-  }
-  get postDate() {
-    return this.#postDate;
-  }
-  set postDate(postDate: Date) {
-    this.#postDate = postDate;
-  }
+  _id: String;
+  title: String;
+  salary: Number;
+  location: Location;
+  description: String;
+  experience: String;
+  skills: String[];
+  postDate: Date;
   constructor(
     id: String,
     title: String,
@@ -97,14 +31,14 @@ export class Jobs {
     skills: String[],
     postDate: Date
   ) {
-    this.#_id = id;
-    this.#title = title;
-    this.#salary = salary;
-    this.#location = location;
-    this.#description = description;
-    this.#experience = experience;
-    this.#skills = skills;
-    this.#postDate = postDate;
+    this._id = id;
+    this.title = title;
+    this.salary = salary;
+    this.location = location;
+    this.description = description;
+    this.experience = experience;
+    this.skills = skills;
+    this.postDate = postDate;
   }
 }
 
